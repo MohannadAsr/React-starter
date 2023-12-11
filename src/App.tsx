65c542e5f615e9ccb187bc8481f497660c6b596c
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2023-12-11 20:26:15 +0300

    Implement site-mode wth context

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './styles/global.scss';
+import { useAppContext } from './App/AppProvider';
 
 function App() {
   const { t, i18n } = useTranslation();
+  const { AppStore, AppDispatch } = useAppContext();
+
   const router = [
     {
       path: '/',
@@ -17,35 +20,41 @@ function App() {
   ];
 
   return (
-    <>
-      <div className=" h-full" style={{ height: '100vh' }}>
-        <NavBar />
-        <div
-          onClick={() =>
-            i18n.changeLanguage(i18n.language == 'ar' ? 'en' : 'ar')
-          }
-        ></div>
-        <div>
-          <h1 className=" text-center text-2xl my-5">
-            {t('Welcome to React')}
-          </h1>
-        </div>
-        <Routes>
-          {router.map((item) => {
-            return (
-              <Route
-                path={item.path}
-                element={item.element}
-                key={Math.random()}
-              />
-            );
-          })}
-          <Route path="contactus" element={<NavBar />}>
-            <Route path=":id" />
-          </Route>
-        </Routes>
+    <div className=" h-full" style={{ height: '100vh' }}>
+      <NavBar />
+      <div
+        onClick={() => i18n.changeLanguage(i18n.language == 'ar' ? 'en' : 'ar')}
+      ></div>
+      <div>
+        <h1 className=" dark:bg-black dark:text-white text-center text-2xl my-5">
+          {t('Welcome to React')}
+        </h1>
       </div>
-    </>
+      <Routes>
+        {router.map((item) => {
+          return (
+            <Route
+              path={item.path}
+              element={item.element}
+              key={Math.random()}
+            />
+          );
+        })}
+        <Route path="contactus" element={<NavBar />}>
+          <Route path=":id" />
+        </Route>
+      </Routes>
+      <span
+        onClick={() =>
+          AppDispatch({
+            type: 'SwitchMode',
+            payload: AppStore.mode === 'dark' ? 'light' : 'dark',
+          })
+        }
+      >
+        {AppStore.mode}
+      </span>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import i18n from './plugins/i18n.ts'; // Import your i18n instance
 import { I18nextProvider } from 'react-i18next';
+import AppProvider from './App/AppProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </I18nextProvider>
   </BrowserRouter>
 );
